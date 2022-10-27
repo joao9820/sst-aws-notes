dcc2dc319c9c286d116ee6e3d2242a4e3550f4d1
joao9820 <joaovictorv9820@gmail.com>
2022-10-27 01:20:43 -0300

    Adiconando uma storage stack

---
+++ stacks/StorageStack.js
@@ -0,0 +1,21 @@
+import {Table, Bucket} from '@serverless-stack/resources';
+/* Stack responsável pelo armazenamento DynamoDB e S3 */
+export function StorageStack({stack, app}){
+
+  const bucket = new Bucket(stack, "Uploads");
+  //Criando tabela Notes
+  const table = new Table(stack, "Notes", {
+    fields: {
+      userId: "string",
+      noteId: "string",
+    },
+    primaryIndex: {partitionKey: "userId", sortKey: "noteId"}
+  });
+
+  //Dessa forma conseguiremos referenciar os itens nas outras stacks
+  return {
+    table,
+    bucket
+  }
+
+}
